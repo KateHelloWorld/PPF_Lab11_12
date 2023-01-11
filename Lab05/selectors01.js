9fe518129720b45bd4893cc3cdb7b8f079bf014b
Tetiana <teti@gmail.com>
2023-01-11 15:57:54 +0100

    fwefwf

---
+++ Lab05/selectors01.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    //zadanie 6.4.1
+    $("#button_01").click(function(){
+        $("p").hide();
+    });
+
+    // zadanie 6.5.1
+    $("#button_02").click(function(){
+        $("#p_03").hide();
+    });
+
+    //zadanie 6.6.1
+    $("#button_03").click(function(){
+        $(".cp_01").hide();
+    })
+    //strona 21
+});
